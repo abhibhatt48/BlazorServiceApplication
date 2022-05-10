0d36a1e00ae3380ede6d4ce4b264d5ad3f5cdff0
Abhishek Bhatt <bhattabhishek499@gmail.com>
2022-05-10 17:50:42 +0530

    added blazor server project

---
+++ BlazorServerCRUD/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServerCRUD.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Game>().HasData(
+                new Game { Id = 1, Name = "xyz", Developer = "abc", Release = new DateTime(2004, 1, 1) },
+                new Game { Id = 2, Name = "sdf", Developer = "xzc", Release = new DateTime(2003, 4, 5) }
+                );
+        }
+
+        public DbSet<Game> Games => Set<Game>();
+    }
+}
